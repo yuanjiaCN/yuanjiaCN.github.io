0e7b2a67100e029f6290b57fddf3c66e12a925a9
yuanjiaCN <cnyuanjia@gmail.com>
2017-07-27 14:43:31 +0800

    change

--- photoAlbum.js
+++ photoAlbum.js
@@ -1,34 +1,59 @@
 $(function () {
-    $("#gardening").mouseover(function () {
+    $(".ita #gardening").mouseover(function () {
         $("#gardening").css({
             background: "rgb(229,116,55)"
         })
         $("#italic").css({
-            background:"rgb(214,238,246)"
+            background: "rgb(214,238,246)"
         })
     })
-    $("#gardening").mouseout(function () {
+    $(".ita #gardening").mouseout(function () {
         $("#italic").css({
-            background:"rgb(56,182,230)"
+            background: "rgb(56,182,230)"
         })
         $("#gardening").css({
-            background:"rgb(214,238,246)"
+            background: "rgb(214,238,246)"
         })
     })
-    $("#gardening").click(function () {
-        $(".gardening").css({
-            display:'block'
+
+
+    $(".gar #italic").mouseover(function () {
+        $("#gardening").css({
+            background: "rgb(214,238,246)"
         })
-        $(".italic").css({
-            display:"none"
-        })
-    })
-    $("#italic").click(function () {
-        $(".gardening").css({
-            display:'none'
-        })
-        $(".italic").css({
-            display:"block"
+        $("#italic").css({
+            background: "rgb(56,182,230)"
         })
     })
-})
+    $(".gar #italic").mouseout(function () {
+        $("#gardening").css({
+            background: "rgb(229,116,55)"
+        })
+        $("#italic").css({
+            background: "rgb(214,238,246)"
+        })
+    })
+
+    $("#album>article>div img").click(function (e) {
+        var $original = $(this).attr("original");
+        $("#show img").attr("src", $original);
+        $("#show").css({
+            width: "100%",
+            height: "100%",
+            background:"rgba(10,10,10,0.3)"
+        })
+        $("#show>div>img").css({
+            display: "block",
+        })
+    })
+    $("#show").click(function () {
+        $("#show").css({
+            width: "0",
+            height: "0"
+        })
+        $("#show>div>img").css({
+            display: "none",
+        })
+    })
+
+    })
