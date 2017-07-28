9c08b9e7496eeee50f30c6421e55801f64c333d0
yuanjiaCN <cnyuanjia@gmail.com>
2017-07-28 20:24:34 +0800

    new

--- content.js
+++ content.js
@@ -72,4 +72,72 @@ $(function () {
             }
         }
     })
-})
+                //其他文章推荐功能
+    $("aside.right nav").append(
+        "<a href='first.html'>轮播图小心得</a>"+
+        "<a href='second.html'>外边距叠加、行框、行内框、outline属性</a>" +
+        "<a href='third.html'>做博客遇到小问题【1】主页</a>" +
+        "<a href='forth.html'>做博客遇到小问题【2】评论区</a>" +
+        "<a href='fifth.html'>CSS创建等高文本列，条件注释,不透明度,神器属性text-overflow,强迫症福利：标点符号悬挂hanging-punctuation</a>"+
+        "<a href='sixth.html'>做博客遇到的小问题【3】溢出、伪选择器</a>" +
+        "<a href='seventh.html'>博客用到的文字换行、首行渲染、CSS3动画属性</a>" +
+        "<a href='eighth.html'>实现不定长度的标题和段落在不同尺寸的图片上在同一个位置水平居中（垂直可调）</a>" +
+        "<a href='ninth.html'>让背景图片浮在图片上方</a>" +
+        "<a href='tenth.html'>transition不支持display;图片下方有一小条背景溢出的处理方法</a>" +
+        "<a href='eleventh.html'>html5、css3表单相关</a>" +
+        "<a href='twelfth.html'>css3媒体查询；A元素hover时改变B元素背景色</a>" +
+        "<a href='thirteenth.html'>图片不拉伸；背景图片随滚轮移动；首行缩进2个中文字</a>"+
+        "<a href='fourteenth.html'>文章批量推荐功能（不包括当前页）;上一篇、下一篇切换功能</a>"+
+        "<a href='fifteenth.html'>在网页内点击链接不跳转切换图片集</a>"
+    );
+
+                    //上一篇、下一篇切换功能
+        var $thisParaName =$(".content>h1").text();
+        var $a = $("aside.right nav").children();
+        for(var i = 0;i<$a.length;i++){
+           if($a[i].innerHTML == $thisParaName) {
+              var $thisParaA = $($a[i]);                     //DOM对象转换为jQuery对象
+              var $nextParaA = $thisParaA.next();
+              var $prevParaA = $thisParaA.prev();
+              var $thisParaHref= $thisParaA.attr("href");    //当前页面的href
+              var $nextParaHref= $nextParaA.attr("href");
+              var $prevParaHref= $prevParaA.attr("href");
+              var $thisParaAName= $thisParaA.text();         //当前页面超链接名
+              var $nextParaAName= $nextParaA.text();         //最后一篇博客的后一篇链接名为""
+              var $prevParaAName= $prevParaA.text();
+
+
+              var cut = $(".cut");
+              if($nextParaAName==""){
+                  $(".cut").append(
+                      "<a href='" + $prevParaHref + "'>上一篇：" + $prevParaAName +"</a>"+
+                      "<br/>"+
+                      "<a href='" + "#" + "'>下一篇：" + "这是最后一篇了" +"</a>"
+                  );
+              }else if($prevParaAName==""){
+                  $(".cut").append(
+                      "<a href='" + "#" + "'>上一篇：" + "这是第一篇" +"</a>"+
+                      "<br/>"+
+                      "<a href='" + $nextParaHref + "'>下一篇：" + $nextParaAName +"</a>"
+                  );
+              }else{
+                  $(".cut").append(
+                      "<a href='" + $prevParaHref + "'>上一篇：" + $prevParaAName +"</a>"+
+                      "<br/>"+
+                      "<a href='" + $nextParaHref + "'>下一篇：" + $nextParaAName +"</a>"
+                  );
+              }
+               //其它文章推荐隐藏当前的文章
+
+               if($thisParaName==$thisParaAName){    //这里detach和remove都可以用，但是empty不可以
+                  $("aside.right nav a").detach("a[href='" + $thisParaHref+ "']");
+               }
+               //if($thisParaName==$thisParaAName){
+               //    $thisParaA.css({                 //更方便，但是上一个代码比较符合这里的语义
+               //        display:"none"
+               //    })
+               //}
+
+               }
+           }
+        })
--- photoAlbum.js
+++ photoAlbum.js
@@ -1,46 +1,48 @@
 $(function () {
-    $(".ita #gardening").mouseover(function () {
+    $("#gardening").mouseover(function () {
         $("#gardening").css({
             background: "rgb(229,116,55)"
         })
-        $("#italic").css({
-            background: "rgb(214,238,246)"
-        })
     })
-    $(".ita #gardening").mouseout(function () {
-        $("#italic").css({
-            background: "rgb(56,182,230)"
-        })
+    $("#gardening").mouseout(function () {
         $("#gardening").css({
             background: "rgb(214,238,246)"
         })
     })
-
-
-    $(".gar #italic").mouseover(function () {
+    $("#gardening").click(function () {
         $("#gardening").css({
-            background: "rgb(214,238,246)"
+           color:"rgb(229,116,55)"
         })
+        $("#italic").css({
+           color: "black"
+        })
+    })
+    $("#italic").mouseover(function () {
         $("#italic").css({
             background: "rgb(56,182,230)"
         })
     })
-    $(".gar #italic").mouseout(function () {
-        $("#gardening").css({
-            background: "rgb(229,116,55)"
-        })
+    $("#italic").mouseout(function () {
         $("#italic").css({
             background: "rgb(214,238,246)"
         })
     })
-
+    $("#italic").click(function () {
+        $("#italic").css({
+            color:"rgb(56,182,230)"
+        })
+        $("#gardening").css({
+            color: "black"
+        })
+    })
+//点开看大图
     $("#album>article>div img").click(function (e) {
         var $original = $(this).attr("original");
         $("#show img").attr("src", $original);
         $("#show").css({
             width: "100%",
             height: "100%",
-            background:"rgba(10,10,10,0.3)"
+            background: "rgba(10,10,10,0.3)"
         })
         $("#show>div>img").css({
             display: "block",
@@ -55,5 +57,22 @@ $(function () {
             display: "none",
         })
     })
-
+//切换图片集
+    var $titlePara = $("#album>article>div .title");
+    var $LI = $("#album>nav li");
+    $LI.click(function () {
+        var $HREF = $(this).children("a").attr("id");
+        for (var i = 0; i < $titlePara.length; i++) {
+            var $title = $($titlePara[i]);
+            if ($title.text() !== $HREF) {
+                $title.parent().parent().parent().css({
+                    display: "none"
+                })
+            } else {
+                $title.parent().parent().parent().css({
+                    display: "block"
+                })
+            }
+        }
     })
+})
