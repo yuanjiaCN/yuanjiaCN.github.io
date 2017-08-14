e686b04f615a3dea20297f8b8094562c3cce62ad
yuanjiaCN <cnyuanjia@gmail.com>
2017-08-14 21:25:02 +0800

    new

--- photoAlbum.js
+++ photoAlbum.js
@@ -37,6 +37,7 @@ $(function () {
     })
 
     $("#album>article").append(
+        "<img date='twentySecond' value='italic' src='picture/italic20(little).jpg' original='picture/original/italic20.jpg' />"+
         "<img date='twentyFourth' value='gardening' src='picture/gardening20(little).jpg' original='picture/original/gardening20.jpg'/>"+
         "<img date='twentyFirst' value='italic' src='picture/italic19(little).jpg' original='picture/original/italic19.jpg' />"+
         "<img date='twentyThird' value='gardening' src='picture/gardening19(little).jpg' original='picture/original/gardening19.jpg'/>"+
