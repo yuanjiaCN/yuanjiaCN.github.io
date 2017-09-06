f526d1c9fff188cf63f0fb7e84fb1d7bce23cf59
yuanjia <cnyuanjia@gmail.com>
2017-09-06 19:53:14 +0800

    JSON1

--- a.js
+++ a.js
@@ -1,6 +1,7 @@
 $(function all () {
  $all =
-     "<a href='specificationSet.html' class='set' value='2017.9.4'>前端代码规范合集</a>"
+       "<a href='fiftyFifth.html' class='blog' value='2017.9.6'>JSON学习(1)</a>"
+     + "<a href='specificationSet.html' class='set' value='2017.9.4'>前端代码规范合集</a>"
      + "<a href='fiftyFourth.html' class='book' value='2017.9.4'>前端代码规范(4)——分号、空格</a>"
      + "<a href='fiftyThird.html' class='book' value='2017.9.3'>前端代码规范(3)——严格模式、变量声明、定义域提升、比较判断、||、&&</a>"
      + "<a href='fiftySecond.html' class='book' value='2017.9.3'>前端代码规范(2)——IIFE</a>"
--- allNavigation.js
+++ allNavigation.js
@@ -4,70 +4,71 @@ $(function () {
         $("#book").mouseover(function () {
             $("#book").css({
                 background: "rgb(229,116,55)"
-            })
-        })
+            });
+        });
         $("#book").mouseout(function () {
             $("#book").css({
                 background: "rgb(221,221,221)"
-            })
-        })
+            });
+        });
         $("#book").click(function () {
             $("#book").css({
                 color: "rgb(229,116,55)"
-            })
+            });
             $("#blog").css({
                 color: "black"
-            })
+            });
             $("#set").css({
                 color: "black"
-            })
-        })
+            });
+        });
         $("#blog").mouseover(function () {
             $("#blog").css({
                 background: "rgb(56,182,230)"
-            })
-        })
+            });
+        });
         $("#blog").mouseout(function () {
             $("#blog").css({
                 background: "rgb(221,221,221)"
-            })
-        })
+            });
+        });
         $("#blog").click(function () {
             $("#blog").css({
                 color: "rgb(56,182,230)"
-            })
+            });
             $("#book").css({
                 color: "black"
-            })
+            });
             $("#set").css({
                 color: "black"
-            })
-        })
+            });
+        });
         $("#set").mouseover(function () {
             $("#set").css({
                 background: "rgb(116,129,55)"
-            })
+            });
             $("#set").css({
                 color: "black"
-            })
-        })
+            });
+        });
         $("#set").mouseout(function () {
             $("#set").css({
                 background: "rgb(221,221,221)"
-            })
-        })
+            });
+        });
         $("#set").click(function () {
             $("#set").css({
                 color: "rgb(116,129,55)"
-            })
+            });
             $("#book").css({
                 color: "black"
-            })
+            });
             $("#blog").css({
                 color: "black"
             })
         })
     }
+
     mouseAndClick();
 
     function importTheAllA() {
@@ -77,7 +78,8 @@ $(function () {
         $("#navigation>article>section.right a").wrap("<div><div class='left'><h1></h1></div></div>");//把超链接包裹起来
         $("#navigation>article>section.right>div").append("<div class='right'><img src='picture/head.jpg'/></div>");
     }
-    importTheAllA()
+
+    importTheAllA();
 
 
     //把A中包含时间的value值传递给p
@@ -105,31 +107,31 @@ $(function () {
         }
         var $L = new Array();//这是当前页面没有被隐藏的blog,如果没有这个的话，分类导航的页数就会联系到全部导航中
         var $Link = $("article>section.right>div");
-        for (j = 0; j<$Link.length; j++) {
-            if($Link[j].style.display !== "none"){
-                $L.push($Link[j]) //把每个看得见的链接push到$L,用[j]=[j],由于看得见的链接不是连续的，会出事情
+        for (j = 0; j < $Link.length; j++) {
+            if ($Link[j].style.display !== "none") {
+                $L.push($Link[j]); //把每个看得见的链接push到$L,用[j]=[j],由于看得见的链接不是连续的，会出事情
             }
         }
         $Link.css({ //把所有链接都隐藏
             display: "none"
-        })
+        });
         for (k = 0; k < 10; k++) {
-            if($L[k]){//如果blog数不满10，会报错
+            if ($L[k]) {//如果blog数不满10，会报错
                 $L[k].style.display = "block";//只显示前10个blog
             }
         }
         clickLinkNumber($L);//把$L传递给这个函数，让它以$L来创建页数
-    })
+    });
     var $allNav = $("#navigation>nav>header");
     $allNav.click(function () {
         $A.parent().parent().parent().css({
             display: "block"
-        })
+        });
         $LI.children("a").css({
             color: "black"
-        })
-        createPage()//重新点回导航也要更新页数
-    })
+        });
+        createPage();//重新点回导航也要更新页数
+    });
 
     function vagueSearch() {
         //增加一个计数器，记录被隐藏的链接数。
@@ -182,21 +184,21 @@ $(function () {
             }
             var $L = new Array();//这是当前页面没有被隐藏的blog,如果没有这个的话，分类导航的页数就会联系到全部导航中
             var $Link = $("article>section.right>div");
-            for (j = 0; j<$Link.length; j++) {
-                if($Link[j].style.display !== "none"){
-                    $L.push($Link[j]) //把每个看得见的链接push到$L,用[j]=[j],由于看得见的链接不是连续的，会出事情
+            for (j = 0; j < $Link.length; j++) {
+                if ($Link[j].style.display !== "none") {
+                    $L.push($Link[j]); //把每个看得见的链接push到$L,用[j]=[j],由于看得见的链接不是连续的，会出事情
                 }
             }
             $Link.css({ //把所有链接都隐藏
                 display: "none"
-            })
+            });
             for (k = 0; k < 10; k++) {
-                if($L[k]){//如果blog数不满10，会报错
+                if ($L[k]) {//如果blog数不满10，会报错
                     $L[k].style.display = "block";//只显示前10个blog
                 }
             }
             clickLinkNumber($L);//把$L传递给这个函数，让它以$L来创建页数
-        })
+        });
 
         //预留文字点击时隐藏
         var $submit = $("#submit");
@@ -206,19 +208,20 @@ $(function () {
                 $(this).val("");
             }
             $("#search>p").hide();
-        })
+        });
         $input.blur(function () {
             if ($(this).val() == "") {
                 $(this).val("请输入关键词");
                 $("#search>p").hide();
             }
-        })
+        });
         //搜索
         var $aHref = new Array();
         for (k = 0; k < $A.length; k++) {
             $aHref.push($A[k]);
         }
     }
+
     vagueSearch();
 
     //创建原始页面
@@ -240,7 +243,7 @@ $(function () {
         }
         $Link.css({//隐藏所有的blog
             display: "none"
-        })
+        });
         for (i = 0; i < 10; i++) {
             $Link[i].style.display = "block";//只显示前10篇blog
         }
@@ -248,12 +251,12 @@ $(function () {
         var $L = new Array();//这是看得见的链接组成的数组
         var $Link = $("article>section.right>div");
         /*没什么用，开始的时候本身就只看得见10篇
-        for (j = 0; j<$Link.length; j++) {
-            if($Link[j].style.display !== "none"){//如果页面本身被隐藏，那就不要算到现有链接中
-                $L[j] = $Link[j];
-            }
-        }
-        */
+         for (j = 0; j<$Link.length; j++) {
+         if($Link[j].style.display !== "none"){//如果页面本身被隐藏，那就不要算到现有链接中
+         $L[j] = $Link[j];
+         }
+         }
+         */
         var liNumber = Math.ceil($L.length / 10);
         for (page = 0; page < liNumber; page++) {
             $pageNumberUl.append(document.createElement("li"));//加上相应数量的li页码
@@ -261,13 +264,13 @@ $(function () {
         }
         var $linkNumber = $("#navigation>article>section.right ul>li");
         var $Link = $("article>section.right>div");
-        for(i = 0; i < $linkNumber.length; i++){
+        for (i = 0; i < $linkNumber.length; i++) {
             $linkNumber[i].onclick = function () {
                 var pageNumber = this.innerHTML;
                 $Link.css({
                     display: "none"
-                })
-                for(k = 10*(pageNumber-1); k < 10 * pageNumber; k++){//获取当前页数相对应的博客
+                });
+                for (k = 10 * (pageNumber - 1); k < 10 * pageNumber; k++) {//获取当前页数相对应的博客
                     $Link[k].style.display = "block";
                 }
             }
@@ -287,14 +290,14 @@ $(function () {
         }
         var $linkNumber = $("#navigation>article>section.right ul>li");
         var $Link = $("article>section.right>div");
-        for(i = 0; i < $linkNumber.length; i++){
+        for (i = 0; i < $linkNumber.length; i++) {
             $linkNumber[i].onclick = function () {
                 var pageNumber = this.innerHTML;
                 $Link.css({
                     display: "none"
-                })
-                for(k = 10*(pageNumber-1); k < 10 * pageNumber; k++){
-                    if($L[k]){ //博客数目不到10*pageNumber时会报错
+                });
+                for (k = 10 * (pageNumber - 1); k < 10 * pageNumber; k++) {
+                    if ($L[k]) { //博客数目不到10*pageNumber时会报错
                         $L[k].style.display = "block";
                     }
 
@@ -302,4 +305,4 @@ $(function () {
             }
         }
     }
-})
+});
