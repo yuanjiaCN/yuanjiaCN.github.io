889d6370dad0196e067a7d48d27587a029fb9761
yuanjia <cnyuanjia@gmail.com>
2017-11-22 22:49:22 +0800

    node.js fs

--- nodeTest/main.js
+++ nodeTest/main.js
@@ -215,7 +215,35 @@
  var fs = require("fs");
  var data = "Hello, Node.js";
  fs.writeFileSync("output1.txt", data);
+
+ var fs = require("fs");
+ var txt = fs.statSync("sample.txt").size;
+ console.log(txt);//31
+ var fs = require("fs");
+ var rs = fs.createReadStream("sample.txt","utf-8");
+ rs.on("data",function (chunk) {
+ console.log("DATA:");
+ console.log(chunk);
+ });
+ rs.on("end",function () {
+ console.log("END");
+ });
+ rs.on("error",function (err) {
+ console.log("error:" + err);
+ });
+ var fs = require("fs");
+ var ws1 = fs.createWriteStream("blank.txt","utf-8");
+ ws1.write("使用Stream写入文本数据...\n");
+ ws1.write("END");
+ ws1.end();
+
+ var ws2 = fs.createWriteStream("blank2.txt");
+ ws2.write(new Buffer("使用Stream写入二进制数据...\n","utf-8"));
+ ws2.write(new Buffer("END","utf-8"));
+ ws2.end();
  */
 var fs = require("fs");
-var txt = fs.statSync("sample.txt").size;
-console.log(txt);//31
+var rs = fs.createReadStream("text1.txt");
+var ws = fs.createWriteStream("text2.txt");
+rs.pipe(ws);
+
