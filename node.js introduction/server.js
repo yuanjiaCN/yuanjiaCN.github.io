d426ae773b445390a9ea4352ba3b5251984a946f
yuanjia <cnyuanjia@gmail.com>
2017-12-04 23:26:11 +0800

    node入门

--- node.js introduction/server.js
+++ node.js introduction/server.js
@@ -1,13 +1,10 @@
 var http = require("http");
 var url = require("url");
-function start() {
+function start(route,handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for" + pathname + " received.");
-        route(handle, pathname);
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello world!");
-        response.end();
+        route(handle, pathname, response);
     }
     http.createServer(onRequest).listen(8080);
     console.log("server has started.")
