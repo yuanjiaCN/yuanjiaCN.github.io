d28ef64e24feb7f9a2d792d2775f7d068dada833
yuanjia <cnyuanjia@gmail.com>
2017-12-04 19:51:40 +0800

    node introduction

---
+++ node.js introduction/router.js
@@ -0,0 +1,9 @@
+function route(pathname) {
+    console.log("About to route a request for" + pathname);
+    if(typeof handle[pathname] === "function"){
+        handle[pathname]();
+    } else {
+        console.log("No request handle found for" + pathname);
+    }
+}
+exports.route = route;
