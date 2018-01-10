b933c094b7a0fa6e793a6ea194efe561b7d1d417
YuanJia <cnyuanjia@gmail.com>
2018-01-10 18:02:37 +0800

    some Vue

--- vue/vue.js
+++ vue/vue.js
@@ -1,9 +1,6 @@
 var vm = new Vue({
     el:"#example-1",
     data:{
-        messageA:"i am A",
-        messageB:"i am B",
-        messageC:"i am C",
-        type : "B"
+        ok:"false"
     }
 });
